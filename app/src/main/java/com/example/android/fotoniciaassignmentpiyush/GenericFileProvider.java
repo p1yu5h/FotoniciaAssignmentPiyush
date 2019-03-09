58101ddf342c24174b1509e1983303d8bce32dc9
p1yu5h <piyush.satija@gmail.com>
2019-03-09 15:34:44 +0530

    Added select images and share options.

---
+++ app/src/main/java/com/example/android/fotoniciaassignmentpiyush/GenericFileProvider.java
@@ -0,0 +1,8 @@
+package com.example.android.fotoniciaassignmentpiyush;
+
+import android.content.ContentProvider;
+import android.support.v4.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider {
+
+}
--- app/src/main/java/com/example/android/fotoniciaassignmentpiyush/MainActivity.java
+++ app/src/main/java/com/example/android/fotoniciaassignmentpiyush/MainActivity.java
@@ -4,14 +4,17 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -34,10 +37,13 @@ public class MainActivity extends AppCompatActivity {
     ImageView element1, element2, base;
     RelativeLayout relativeLayout;
     Bitmap image;
-    Button save;
+    Button save, selectImages, share;
 
     private int xDelta;
     private int yDelta;
+
+    private static final int SELECT_PICTURE = 100;
+    private static int image_set = 0;
 
 
     @Override
@@ -45,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
         element1 = findViewById(R.id.element1);
         element2 = findViewById(R.id.element2);
         base = findViewById(R.id.base_layout);
@@ -58,6 +64,18 @@ public class MainActivity extends AppCompatActivity {
 
         element1.setOnTouchListener(onTouchListener());
         element2.setOnTouchListener(onTouchListener());
+
+        selectImages = findViewById(R.id.select_images);
+        share = findViewById(R.id.share);
+
+        selectImages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImageChooser();
+            }
+        });
+
+
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +91,23 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }, 1000);
 
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = "Look at my awesome picture";
+                File imageFile = new File(Environment.getExternalStorageDirectory()+"/piyushtest1.png");
+                Log.d("hellll", Environment.getExternalStorageDirectory()+"/piyushtest1.png");
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".com.example.android.fotoniciaassignmentpiyush.provider", imageFile);
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, text);
+                shareIntent.putExtra(Intent.EXTRA_STREAM, photoURI);
+                shareIntent.setType("image/*");
+                shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                startActivity(Intent.createChooser(shareIntent, "Share images..."));
             }
         });
 
@@ -159,4 +194,67 @@ public class MainActivity extends AppCompatActivity {
         }finally {
         }
     }
+
+    void openImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (resultCode == RESULT_OK) {
+                    if (requestCode == SELECT_PICTURE) {
+                        // Get the url from data
+                        final Uri selectedImageUri = data.getData();
+                        if (null != selectedImageUri) {
+                            // Get the path from the Uri
+                            String path = getPathFromURI(selectedImageUri);
+                            Log.i("piyush", "Image Path : " + path);
+                            // Set the image in ImageView
+                            if(image_set == 0){
+                            findViewById(R.id.element1).post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ((ImageView) findViewById(R.id.element1)).setImageURI(selectedImageUri);
+                                }
+                            });
+                            image_set = 1;
+                            }else {
+                                findViewById(R.id.element2).post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        ((ImageView) findViewById(R.id.element2)).setImageURI(selectedImageUri);
+                                    }
+                                });
+                            image_set = 0;
+                            }
+
+
+
+                        }
+                    }
+                }
+            }
+        }).start();
+
+    }
+
+    /* Get the real path from the URI */
+    public String getPathFromURI(Uri contentUri) {
+        String res = null;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if (cursor.moveToFirst()) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+        }
+        cursor.close();
+        return res;
+    }
+
 }
